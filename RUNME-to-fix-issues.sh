827e7f0bc678d7b22ec48261e9a1fd277150567d
Jon Crall <crallj@rpi.edu>
2013-04-02 12:52:29 -0400

    script tweak

--- RUNME-to-fix-issues.sh
+++ RUNME-to-fix-issues.sh
@@ -1,6 +1,21 @@
 export hs_git_host=git@hyrule.cs.rpi.edu:
 #export hs_git_host=https://github.com/Erotemic/
 
-export hs_tpl_repo=tpls-hotspotter
+export hs_tpl_repo=tpl-hotspotter
 
+git submodule update --init
+git submodule init 
+git submodule update
+
+git submodule init
 git submodule add $hs_git_host$hs_tpl_repo.git tpl
+
+# To remove submodules
+if ["False" == "True"]
+    export submodulepath=tpl
+    git config -f .git/config --remove-section submodule.$submodulepath
+    git config -f .gitmodules --remove-section submodule.$submodulepath
+    git rm --cached $submodulepath
+    rm -rf $submodulepath
+    rm -rf .git/modules/$submodulepath
+fi

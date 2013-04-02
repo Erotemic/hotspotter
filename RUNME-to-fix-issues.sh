672480038483b7e5f7dec59143eaa5444dad0ba0
Jon Crall <crallj@rpi.edu>
2013-04-02 10:36:24 -0400

    removed conflicting tpl directory

--- RUNME-to-fix-issues.sh
+++ RUNME-to-fix-issues.sh
@@ -1,1 +1,6 @@
-git submodule add git@hyrule.cs.rpi.edu:tpl-hotspotter.git tpl
+export hs_git_host=git@hyrule.cs.rpi.edu:
+#export hs_git_host=https://github.com/Erotemic/
+
+export hs_tpl_repo=tpls-hotspotter
+
+git submodule add $hs_git_host$hs_tpl_repo.git tpl

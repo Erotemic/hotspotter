d76a56652912d76a43749081424aab51b246d2c4
Jon Crall <crallj@rpi.edu>
2013-06-25 00:52:33 -0400

    Worked on cv2 integration

--- hotspotter/ChipFunctions.py
+++ hotspotter/ChipFunctions.py
@@ -1,4 +1,5 @@
 import os.path
+import cv2
 from hotspotter.algo.imalgos import histeq
 from hotspotter.algo.imalgos import adapt_histeq
 from hotspotter.algo.imalgos import contrast_stretch
@@ -74,6 +75,8 @@ def compute_chip_driver(hs, cx, showmsg=True):
         chip_fpath = hs.iom.get_chip_fpath(cid)
         chip_fname = os.path.split(chip_fpath)[1]
         logmsg(('\nComputing Chip: cid=%d fname=%s\n'+hs.am.get_algo_name(['preproc'])) % (cid, chip_fname))
+
+
     compute_chip(*cc_args)
 
 
@@ -107,11 +110,8 @@ def read_text_chiprep_file(outname):
         return (fpts, fdsc)
     
 # TODO: orientation is currently a hack, should be computed by chips not chiprep
-def compute_chiprep(chip_fpath, chiprep_fpath, exename, orientation):
-    
-    chip_unorient = Image.open(chip_fpath) 
-    angle_degrees = orientation * 180 / np.pi
-    chip_orient = chip_unorient.rotate(angle_degrees, resample=Image.BICUBIC, expand=1)
+def compute_chiprep2(chip_fpath, chiprep_fpath, exename, orientation):
+    chip_orient = read_oriented_chip(chip_fpath, orientation)
     orientchip_fpath = chip_fpath +'rotated.png'
     chip_orient.save(orientchip_fpath, 'PNG')
     outname = orientchip_fpath + '.hesaff.sift'
@@ -125,7 +125,47 @@ def compute_chiprep(chip_fpath, chiprep_fpath, exename, orientation):
         raise Exception('  * The output file doesnt exist: '+outname)
     fpts, fdsc = read_text_chiprep_file(outname)
     np.savez(chiprep_fpath, fpts, fdsc)
-    return out
+
+def read_oriented_chip(chip_fpath, orientation):
+    chip_unorient = Image.open(chip_fpath) 
+    chip_orient = chip_unorient.rotate(orientation * 180 / np.pi,
+                                       resample=Image.BICUBIC, expand=1)
+    return chip_orient
+
+def normalize(array, dim=0):
+    array_max = array.max(dim)
+    array_min = array.min(dim)
+    array_exnt = np.subtract(array_max, array_min)
+    return np.divide(np.subtract(array, array_min), array_exnt)
+
+def compute_chiprep(chip_fpath, chiprep_fpath, exename, orientation):
+    # Read image and convert to grayscale
+    #img_ = cv2.imread(chip_fpath)
+    #img = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    img = np.asarray(read_oriented_chip(chip_fpath, orientation))
+    # Create feature detectors / extractors
+    fpts_detector_ = cv2.FeatureDetector_create('SURF')
+    fpts_detector  = cv2.GridAdaptedFeatureDetector(fpts_detector_, 50) # max number of features
+    fdcs_extractor = cv2.DescriptorExtractor_create("SURF")
+
+    # Get cv_dsc and cv_fpts
+    cv_fpts_ = fpts_detector.detect(img)  
+    (cv_fpts, cv_fdsc) = fdcs_extractor.compute(img, cv_fpts_)
+    cv_fdsc2 = normalize(np.array([np.sqrt(dsc) for dsc in normalize(cv_fdsc)]))
+    fdsc = np.array([np.round(255*dsc) for dsc in cv_fdsc2],
+                    dtype=np.uint8)
+    
+    # Get fpts
+    xy_list = np.array([cv_kp.pt for cv_kp in cv_fpts])
+    #diag_shape_list = [[np.cos(cv_kp.angle)*tau/360] for cv_kp in cv_fpts]  # tauday.com
+    diag_shape_list  = [np.cos(cv_kp.angle)*np.pi/180 for cv_kp in cv_fpts]
+    shape_list = [np.array((d, 0, d)) for d in diag_shape_list]
+    scale_list = [cv_kp.size for cv_kp in cv_fpts]
+    #octave_list = [cv_kp.octave for cv_kp in cv_fpts]
+    fpts_ = np.hstack((xy_list, shape_list))
+    fpts = np.array(fpts_, dtype=np.float16)
+    np.savez(chiprep_fpath, fpts, fdsc)
+
 
 def compute_chiprep_args(hs, cx):
     cid           = hs.cm.cx2_cid[cx]

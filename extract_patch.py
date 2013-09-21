3b084ff6c35d68daa912c000c6850a932aaaf0b1
joncrall <crallj@rpi.edu>
2013-09-21 09:32:36 -0400

    warping works

--- extract_patch.py
+++ extract_patch.py
@@ -34,22 +34,9 @@ def quantize_to_pixel_with_offset(z, radius, low, high):
     z_radius = np.ceil(z - iz1)
     return iz1, iz2, z_radius
 
-def get_subchip(rchip, kp, radius):
-    x, y, a, c, d = kp[0]
-    (chip_h, chip_w) = rchip.shape[0:2]
-    ix1, ix2, xm = quantize_to_pixel_with_offset(x, radius, 0, chip_w)
-    iy1, iy2, ym = quantize_to_pixel_with_offset(y, radius, 0, chip_h)
-    subchip = rchip[iy1:iy2, ix1:ix2]
-    subkp = kp.copy() # subkeypoint in subchip coordinates
-    subkp[0,0:2] = (xm, ym)
-    return subchip, subkp
 
 def show_feature(rchip, kp, subkp, **kwargs):
     df2.figure(**kwargs)
-    df2.imshow(rchip, plotnum=(1,2,1), **kwargs)
-    df2.draw_kpts2(kp, ell_color=(1,0,0), pts=True)
-    df2.imshow(subchip, plotnum=(2,2,2), **kwargs)
-    df2.draw_kpts2(subkp, ell_color=(1,0,0), pts=True)
 
 def target_dsize(img, M):
     # Get img bounds under transformation
@@ -62,200 +49,97 @@ def target_dsize(img, M):
     dsize = tuple(map(int, np.ceil((Mw, Mh))))
     return dsize, tx, ty
 
-def warp_image(img, M):
-    img_size = img.shape[0:2]
-    # Find the target warped img extent, add any tranlations
-    dsize, tx, ty = target_dsize(img, M)
-    M = M.copy()
-    M[0,2] += tx
-    M[1,2] += ty
-    print('warp %r -> %r' % (img_size, dsize))
-    #warp_img = cv2.warpAffine(img, M[0:2], dsize, **__cv2_warp_kwargs())
-    warp_img = cv2.warpPerspective(img, M, dsize, **__cv2_warp_kwargs())
-    return warp_img, tx, ty, M
+def warp_image(img, kp, sf):
+    (x, y, a, c, d) = kp[0]
+    A = np.array([[a,  0, 0],
+                  [c,  d, 0],
+                  [0,  0, 1]])
+    T = np.array([[1, 0, -x+1],
+                  [0, 1, -y+1],
+                  [0, 0,    1]])
+    S = np.array([[sf, 0, sf/2],
+                  [0, sf, sf/2],
+                  [0,  0,    1]])
+    img_h, img_w = img.shape[0:2]
+    dsize = np.array(np.ceil(np.array([sf, sf])), dtype=int)
+    print(dsize)
+    Z = S.dot(A).dot(T)
+    print("Z=\n"+repr(A))
+    warp_img = cv2.warpAffine(img, Z[0:2], tuple(dsize), **__cv2_warp_kwargs())
+    #warp_img = cv2.warpPerspective(img, M, dsize, **__cv2_warp_kwargs())
+    return warp_img
 
-
-if not 'hs' in vars():
-    hs = ld2.HotSpotter()
-    hs.load_all(params.GZ)
-    qcx = 111
-    cx = 305
-    # Database descriptor + keypoints
-    cx2_desc = hs.feats.cx2_desc
-    cx2_kpts = hs.feats.cx2_kpts
-    cx2_rchip_size = hs.get_cx2_rchip_size()
-    def get_features(cx):
-        rchip = hs.get_chip(cx)
-        rchip_size = cx2_rchip_size[cx]
-        fx2_kp   = cx2_kpts[cx]
-        fx2_scale = sv2.keypoint_scale(fx2_kp)
-        fx2_desc = cx2_desc[cx]
-        return rchip, rchip_size, fx2_kp, fx2_scale, fx2_desc
-    # Query features
-    rchip1, rchip_size1, fx2_kp1, fx2_scale1, fx2_desc1 = get_features(qcx)
-    # Result features
-    rchip2, rchip_size2, fx2_kp2, fx2_scale2, fx2_desc2 = get_features(cx)
-    # Vsmany index
-    vsmany_index = hs.matcher._Matcher__vsmany_index
-    #c2.precompute_index_vsmany(hs)
-    #qcx2_res = mc2.run_matching(hs)
-
-    #params.__MATCH_TYPE__ = 'bagofwords'
-    #hs.load_matcher()
-    #resBOW = mc2.build_result_qcx(hs, qcx)
-    #df2.show_match_analysis(hs, resBOW, N=5, fignum=1)
-    
-    params.__MATCH_TYPE__ = 'vsmany'
-    hs.load_matcher()
-    params.__VSMANY_SCORE_FN__ = 'LNRAT'
-    resLNRAT = mc2.build_result_qcx(hs, qcx)
-    df2.show_match_analysis(hs, resLNRAT, N=5, fignum=1)
-
-    params.__VSMANY_SCORE_FN__ = 'LNBNN'
-    resLNBNN = mc2.build_result_qcx(hs, qcx)
-    df2.show_match_analysis(hs, resLNBNN, N=5, fignum=2)
-
-    params.__VSMANY_SCORE_FN__ = 'RATIO'
-    resRATIO = mc2.build_result_qcx(hs, qcx)
-    df2.show_match_analysis(hs, resRATIO, N=5, fignum=3)
-
-    params.__VSMANY_SCORE_FN__ = 'RATIO'
-    
-    params.__MATCH_TYPE__ = 'vsone'
-    hs.load_matcher()
-    res_vsone = mc2.build_result_qcx(hs, qcx, use_cache=True)
-    df2.show_match_analysis(hs, res_vsone, N=5, fignum=4)
-    df2.present()
-
-    #allres = init_allres(hs, qcx2_res, SV, oxford=oxford)
-
-    def get_vsmany_all_data():
-        vsmany_all_assign = mc2.assign_matches_vsmany(qcx, cx2_desc, vsmany_index)
-        cx2_fm, cx2_fs, cx2_score = vsmany_all_assign
-        vsmany_all_svout = mc2.spatially_verify_matches(qcx, cx2_kpts, cx2_rchip_size, cx2_fm, cx2_fs)
-        return vsmany_all_assign, vsmany_all_svout
-
-    def get_vsmany_data(vsmany_all_data, cx):
-        ' Assigned matches (vsmany)'
-        vsmany_all_assign, vsmany_all_svout = vsmany_all_data
-        vsmany_cx_assign = map(lambda _: _[cx],  vsmany_all_assign)
-        vsmany_cx_svout  = map(lambda _: _[cx],  vsmany_all_svout)
-        return vsmany_cx_assign, vsmany_cx_svout
-
-    def get_vsone_data(cx):
-        ' Assigned matches (vsone)'
-        vsone_flann, checks = mc2.get_vsone_flann(fx2_desc1)
-        fm, fs = mc2.match_vsone(fx2_desc2, vsone_flann, checks)
-        fm_V, fs_V, H = mc2.spatially_verify(fx2_kp1, fx2_kp2, rchip_size2, fm, fs, qcx, cx)
-        score = fs.sum(); score_V = fs_V.sum()
-        vsone_cx_assign = fm, fs, score
-        vsone_cx_svout = fm_V, fs_V, score_V
-        return vsone_cx_assign, vsone_cx_svout
-
-    # Assign + Verify
-    params.__USE_CHIP_EXTENT__ = False
-    vsmany_all_data = get_vsmany_all_data()
-    vsmany_data = get_vsmany_data(vsmany_all_data, cx)
-    vsone_data = get_vsone_data(cx)
-
-    params.__USE_CHIP_EXTENT__ = True
-    vsmany_all_data2 = get_vsmany_all_data()
-    vsmany_data2 = get_vsmany_data(vsmany_all_data2, cx)
-    vsone_data2 = get_vsone_data(cx)
-
-    def show_matchers_compare(vsmany_data, vsone_data, fignum=0, figtitle=''):
-        vsmany_cx_assign, vsmany_cx_svout = vsmany_data
-        vsone_cx_assign, vsone_cx_svout = vsone_data
-        # Show vsmany
-        fm, fs, score = vsmany_cx_assign
-        fm_V, fs_V, score_V = vsmany_cx_svout
-        plot_kwargs = dict(all_kpts=False, fignum=fignum)
-        df2.show_matches2(rchip1, rchip2, fx2_kp1, fx2_kp2, fm, fs,
-                          plotnum=(2,2,1), title='vsmany assign', **plot_kwargs)
-        df2.show_matches2(rchip1, rchip2, fx2_kp1, fx2_kp2, fm_V, fs_V,
-                          plotnum=(2,2,2),  title='vsmany verified', **plot_kwargs)
-        # Show vsone
-        fm, fs, score      = vsone_cx_assign
-        fm_V, fs_V, score_V = vsone_cx_svout
-        df2.show_matches2(rchip1, rchip2, fx2_kp1, fx2_kp2, fm, fs,
-                          plotnum=(2,2,3), title='vsone assign', **plot_kwargs)
-        df2.show_matches2(rchip1, rchip2, fx2_kp1, fx2_kp2, fm_V, fs_V,
-                          plotnum=(2,2,4), title='vsone verified', **plot_kwargs)
-        df2.set_figtitle(figtitle)
-
-    show_matchers_compare(vsmany_data, vsone_data, fignum=1, figtitle='kpt extent')
-    show_matchers_compare(vsmany_data2, vsone_data2, fignum=2, figtitle='chip extent')
+if __name__ == '__main__':
+    if not 'hs' in vars():
+        hs = ld2.HotSpotter()
+        hs.load_all(params.GZ)
+        qcx = 111
+        cx = 305
+        # Database descriptor + keypoints
+        cx2_desc = hs.feats.cx2_desc
+        cx2_kpts = hs.feats.cx2_kpts
+        cx2_rchip_size = hs.get_cx2_rchip_size()
+        def get_features(cx):
+            rchip = hs.get_chip(cx)
+            rchip_size = cx2_rchip_size[cx]
+            fx2_kp   = cx2_kpts[cx]
+            fx2_scale = sv2.keypoint_scale(fx2_kp)
+            fx2_desc = cx2_desc[cx]
+            return rchip, rchip_size, fx2_kp, fx2_scale, fx2_desc
+        # Query features
+        rchip1, rchip_size1, fx2_kp1, fx2_scale1, fx2_desc1 = get_features(qcx)
+        # Result features
+        rchip2, rchip_size2, fx2_kp2, fx2_scale2, fx2_desc2 = get_features(cx)
+        res = mc2.build_result_qcx(hs, qcx)
+    fig = df2.show_match_analysis(hs, res, N=5, fignum=1)
     df2.update()
 
-    fx2_feature = hs.get_feature_fn(qcx)
-
-def show_feature_fx(fx):
     rchip = rchip1
-    kp, scale, radius, desc = fx2_feature(fx)
-    subchip, subkp = get_subchip(rchip, kp, radius)
-    show_feature(rchip, kp, subkp, fignum=fx)
-    df2.update()
+    fx2_kp = fx2_kp1
+    fx = 200#2294
+    kp = fx2_kp1[fx:fx+1]
+    scale = fx2_scale1[fx]
+    desc = fx2_desc1[fx]
+    #(kp, scale, radius, desc) = fx2_feature(fx)
+    def get_subchip(rchip, kp, radius):
+        x, y, a, c, d = kp[0]
+        (chip_h, chip_w) = rchip.shape[0:2]
+        ix1, ix2, xm = quantize_to_pixel_with_offset(x, radius, 0, chip_w)
+        iy1, iy2, ym = quantize_to_pixel_with_offset(y, radius, 0, chip_h)
+        subchip = rchip[iy1:iy2, ix1:ix2]
+        subkp = kp.copy() # subkeypoint in subchip coordinates
+        subkp[0,0:2] = (xm, ym)
+        return subchip, subkp
+    sf = np.sqrt(scale)*3*np.sqrt(3)
+    subchip, subkp = get_subchip(rchip, kp, sf)
+    warp_subchip   = warp_image(rchip, kp, sf)
+    unit_circle = np.array([(sf/2, sf/2, 1/sf, 0., 1/sf)])
 
-import sys
-sys.exit(1)
+    # Plot chosen keypoint
+    fig = df2.figure(9001, plotnum=(2,2,1))
 
-rchip = rchip1
-fx2_kp = fx2_kp1
-fx = 2294
-(kp, scale, radius, desc) = fx2_feature(fx)
-subchip, subkp = get_subchip(rchip, kp, radius)
+    df2.imshow(rchip, plotnum=(1,3,1))
+    df2.draw_kpts2(kp, ell_color=(1,0,0), pts=True)
 
-show_feature(rchip, kp, subkp, fignum=fx)
+    df2.imshow(subchip, plotnum=(1,3,2))
+    df2.draw_kpts2(subkp, ell_color=(1,0,0), pts=True)
 
-(x, y, a, c, d) = kp[0]
-# Transformation from ellipse to a unit circle
-A = np.array([(a, 0, 0),
-              (c, d, 0),
-              (0, 0, 1)])
-# Scale up so we can see the keypoint
-sf = 1000
-S = np.array([(sf*3,  0, 0), 
-              ( 0, sf*3, 0),
-              ( 0,  0, 1)])
+    df2.imshow(warp_subchip, plotnum=(1,3,3))
+    df2.draw_kpts2(unit_circle, ell_color=(1,0,0), pts=True)
 
-print('A')
-print A
-A = A.dot(S)
-print A
+    df2.set_figtitle('chosen keypoint')
 
-fx2_kpIS = sv2.sqrt_inv(fx2_kp)
-kpIS = fx2_kpIS[fx]
-print('warp subchip')
-#subchip2 = np.swapaxes(subchip, 0, 1)
-warp_subchip, tx, ty, M = warp_image(subchip, A)
-#warp_subchip = np.swapaxes(warp_subchip, 0, 1)
+    # Plot full, cropped, warp
+    df2.draw()
+    #----
+    #fig = df2.figure(fignum=10)
+    #ax = df2.plt.gca()
+    #ax.set_xlim(-1,1)
+    #ax.set_ylim(-1,1)
+    #ax.set_aspect('equal')
+    ##---
+    #df2.draw_sift(desc)
 
-print('warp_subchip.shape = %r ' % (warp_subchip.shape,))
-#circle_a = 3/(np.sqrt(3*sf))
-xm = subkp[0,0]
-ym = subkp[0,1]
-[Axm, Aym,_]  = M.dot(np.array([[xm],[ym],[1]])).flatten()
-
-circle_a = 1/sf
-#(sf*3*np.sqrt(3))
-unit_circle = np.array([(Axm, Aym, circle_a, 0., circle_a)])
-
-# Plot full, cropped, warp
-
-
-df2.imshow(warp_subchip, plotnum=(2,2,4), fignum=fx)
-df2.draw_kpts2(unit_circle, ell_color=(1,0,0), pts=True)
-
-#----
-
-#fig = df2.figure(fignum=10)
-#ax = df2.plt.gca()
-#ax.set_xlim(-1,1)
-#ax.set_ylim(-1,1)
-#ax.set_aspect('equal')
-##---
-#df2.draw_sift(desc)
-
-exec(df2.present())
-#import sys
-#sys.exit(1)
+    exec(df2.present())
+    #import sys
+    #sys.exit(1)

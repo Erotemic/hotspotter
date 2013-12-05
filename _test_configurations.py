718ad39ae08362f01f144dc99d8d32acca29a9ba
joncrall <crallj@rpi.edu>
2013-12-05 09:35:12 -0500

    wip

--- _test_configurations.py
+++ _test_configurations.py
@@ -152,12 +152,12 @@ vsmany_k.update({
 
 vsmany_big_social = vsmany_best.copy()
 vsmany_big_social.update({
-    'K'              : [5, 10, 20], #30], #5, 10],
+    'K'              : [5, 10],#, 20], #30], #5, 10],
     'Knorm'          : [1, 3], #2, 3],
     'Krecip'         : [0],#, 5], #, 5, 10],
     'lnbnn_weight'   : [1], # 1,]
     #'roidist_thresh' : [None, .5], # .5,] 
-    'score_method' : ['pl', 'plw', 'csum'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
+    'score_method' : ['pl', 'plw', 'csum', 'borda', 'bordaw'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
 })
 
 

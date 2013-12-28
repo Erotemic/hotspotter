d07b2414f73f618396cda714cbfa5b6164b6649a
joncrall <crallj@rpi.edu>
2013-12-27 23:20:01 -0500

    wip

---
+++ _tpl/localize.py
@@ -0,0 +1,26 @@
+from os.path import expanduser, join
+from hotspotter import helpers
+
+# Localize hessian affine code
+code_dir   = join(expanduser('~'), 'code')
+hsdir      = join(code_dir, 'hotspotter')
+extern_dir = join(hsdir, '_tpl', 'extern_feat')
+hesaffsrc_dir = join(code_dir, 'ell_desc')
+
+hesaffbuild_dir = join(hesaffsrc_dir, 'build')
+filemap = {
+    hesaffbuild_dir: ['hesaffexe.exe', 'libhesaff.dll'],
+    hesaffsrc_dir: ['pyhesaff.py']
+}
+
+for srcdir, fname_list in filemap.iteritems():
+    for fname in fname_list:
+        src  = join(srcdir, fname)
+        dest = join(extern_dir, fname)
+        try:
+            helpers.copy(src, dest)
+        except Exception as ex:
+            print(ex)
+
+
+raw_input('Press enter to continue')

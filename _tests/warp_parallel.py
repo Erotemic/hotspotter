6b5ec6d435318cc35684bc24a834883c4c270846
Jon Crall <crallj@rpi.edu>
2014-01-13 11:28:25 -0500

    wip

--- _tests/warp_parallel.py
+++ _tests/warp_parallel.py
@@ -1,15 +1,25 @@
-from __future__ import division, print_function
 '''
 There is an issue with cv2.warpAffine on macs.
 This is a test to further investigate the issue.
 '''
+from __future__ import division, print_function
+import os
+import sys
+from os.path import dirname, join
+
+
+def ensure_hotspotter():
+    hotspotter_dir = join(expanduser('~'), 'code', 'hotspotter')
+    if not exists(hotspotter_dir):
+        print('[jon] hotspotter_dir=%r DOES NOT EXIST!' % (hotspotter_dir,))
+    hotspotter_location = split(hotspotter_dir)[0]
+    sys.path.append(hotspotter_location)
+ensure_hotspotter()
 from hotspotter.dbgimport import *  # NOQA
 from hotspotter import chip_compute2 as cc2
 from hotspotter import Parallelize as parallel
 from hotspotter import helpers
 from hotspotter import fileio as io
-from os.path import dirname, join
-import os
 import cv2
 parallel_compute = parallel.parallel_compute
 

9efbae2a09e5684639edd9185467e4ee9d00ef1d
Jon Crall <crallj@rpi.edu>
2014-01-13 11:38:27 -0500

    wip

--- _tests/local_info.py
+++ _tests/local_info.py
@@ -1,4 +1,14 @@
 from __future__ import division, print_function
+
+
+def ensure_hotspotter():
+    hotspotter_dir = join(expanduser('~'), 'code', 'hotspotter')
+    if not exists(hotspotter_dir):
+        print('[jon] hotspotter_dir=%r DOES NOT EXIST!' % (hotspotter_dir,))
+    hotspotter_location = split(hotspotter_dir)[0]
+    sys.path.append(hotspotter_location)
+ensure_hotspotter()
+
 from hotspotter.dbgimport import *  # NOQA
 
 helpers.printvar2('cv2.__version__')

c1e0ad7c6723730894ff4fa87219bbaf7feb3ede
Jon Crall <crallj@rpi.edu>
2014-01-28 17:30:16 -0500

    Added net and profiler

---
+++ cython_compile.sh
@@ -0,0 +1,3 @@
+cython hotspotter/spatial_verification2.py
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -o hotspotter/spatial_verification2.so hotspotter/spatial_verification2.c
+
---
+++ profiler.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Globals
+export TIMESTAMP=$(date -d "today" +"%Y-%m-%d_%H-%M-%S")
+export RAW_SUFFIX=raw.prof
+export CLEAN_SUFFIX=clean.prof
+
+# Input
+export pyscript=$1
+# Output Stage 1
+export line_profile_output=$pyscript.lprof
+# Output Stage 2
+export raw_profile=$pyscript.$TIMESTAMP.$RAW_SUFFIX
+export clean_profile=$pyscript.$TIMESTAMP.$CLEAN_SUFFIX
+
+echo "Profiling $pyscript"
+# Line profile the python code w/ command line args
+kernprof.py -l $@
+# Dump the line profile output to a text file
+python -m line_profiler $line_profile_output >> $raw_profile
+# Clean the line profile output
+python __profiler_cleaner.py $raw_profile $clean_profile
+# Print the cleaned output
+cat $clean_profile
+
+remove_profiles()
+{
+    rm *.profile.txt
+}

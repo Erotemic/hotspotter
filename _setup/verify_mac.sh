319784378e28d0bdbcade3ebe3e21c843dc329e1
Jon Crall <crallj@rpi.edu>
2014-02-04 17:39:15 -0500

    wip

--- _setup/verify_mac.sh
+++ _setup/verify_mac.sh
@@ -1,2 +1,12 @@
 cd ~/code/hotspotter
-otool -L ~/dist/HotSpotter.app/Contents/MacOSX/hstpl/extern_feat/libhesaff.dylib
+# Check to make sure all dylibs have been otooled correctly
+otool -L dist/HotSpotter.app/Contents/MacOS/hstpl/extern_feat/libhesaff.dylib
+otool -L dist/HotSpotter.app/Contents/MacOS/libopencv_highgui.2.4.dylib
+otool -L dist/HotSpotter.app/Contents/MacOS/libflann.dylib
+
+# Check to make sure it runs
+./dist/HotSpotter.app/Contents/MacOS/HotSpotterApp
+
+cd dist 
+open HotSpotter.app
+

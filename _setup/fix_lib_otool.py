65fcd2be4fa84ea8ad6aa41875f03bf0af419a4c
Jon Crall <crallj@rpi.edu>
2014-02-04 11:31:57 -0500

    Cleaned the fix otool script.

---
+++ _setup/fix_lib_otool.py
@@ -0,0 +1,67 @@
+#!/usr/env python
+import subprocess
+import re
+import shutil
+import os
+from os.path import exists, expanduser, join, split, splitext
+
+
+def ensuredir(path):
+    if not exists(path):
+        os.makedirs(path)
+
+
+def _cmd(*args):
+    print(' '.join(args))
+    subprocess.check_output(args)
+
+
+def extract_dependent_dylibs(dylib_fpath, filter_regex=None):
+    'Extracts the dependent libraries of the input dylib'
+    out = subprocess.check_output(['otool', '-L', dylib_fpath])
+    out = [line.strip() for line in out.split('\n')]
+    if filter_regex is not None:
+        out = filter(lambda line: re.search(filter_regex, line), out)
+    dylib_list = [line.split(' ')[0] for line in out]
+    return dylib_list
+
+
+def append_suffix(fpath, suffix):
+    'appends sufix like /some/filename<suffix>.ext'
+    root, fname = split(fpath)
+    name, ext = splitext(fname)
+    new_fname = name + suffix + ext
+    new_fpath = join(root, new_fname)
+    return new_fpath
+
+
+def make_distributable_dylib(dylib_fpath):
+    'removes absolute paths from dylibs on mac using otool'
+    print('[otool] making distributable: %r' % dylib_fpath)
+    assert exists(dylib_fpath)
+    output_dir = split(dylib_fpath)[0]
+    depends_list = extract_dependent_dylibs(dylib_fpath, filter_regex='opencv')
+
+    # Build task list
+    copy_list = []
+    instname_list = []
+    for fpath_src in depends_list:
+        fname = split(fpath_src)[1]
+        fpath_dst = join(output_dir, fname)
+        copy_list.append((fpath_src, fpath_dst))
+        loader_dst = join('@loader_path', fname)
+        instname_cmd = ['install_name_tool', '-change', fpath_src, loader_dst, dylib_fpath]
+        instname_list.append(instname_cmd)
+
+    # Copy the dependencies to the dylib location
+    for (fpath_src, fpath_dst) in copy_list:
+        shutil.copy(fpath_src, fpath_dst)
+
+    # Change the dependencies in the dylib
+    for instname_cmd in instname_list:
+        _cmd(instname_cmd)
+
+if __name__ == '__main__':
+    # input dylib
+    dylib_fpath  = expanduser('~/code/hotspotter/hstpl/extern_feat/libhesaff.dylib')
+    make_distributable_dylib(dylib_fpath)
--- experiment_configs.py
+++ experiment_configs.py
@@ -1,145 +1,122 @@
 from __future__ import division, print_function
 
 vsmany_2 = {
-    'query_type'     : ['vsmany'],
-    'checks'         : [1024],#, 8192],
-    'K'              : [5], #5, 10],
-    'Knorm'          : [1], #2, 3],
-    'Krecip'         : [0], #, 5, 10],
-    'roidist_weight' : [0], # 1,]
-    'recip_weight'   : [0], # 1,]
-    'bursty_weight'  : [0], # 1,]
-    'ratio_weight'   : [0,1], # 1,]
-    'lnbnn_weight'   : [0,1], # 1,]
-    'lnrat_weight'   : [0,1], # 1,]
-    'roidist_thresh' : [None], # .5,]
-    'recip_thresh'   : [0], # 0
-    'bursty_thresh'  : [None], #
-    'ratio_thresh'   : [None], # 1.2, 1.6
-    'lnbnn_thresh'   : [None], #
-    'lnrat_thresh'   : [None], #
-    'nShortlist'   : [1000],
-    'sv_on'        : [True], #True, False],
-    'score_method' : ['csum'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
-    'max_alts'     : [1000],
+    'query_type':      ['vsmany'],
+    'checks':          [1024],  # , 8192],
+    'K':               [5],  # 5, 10],
+    'Knorm':           [1],  # 2, 3],
+    'Krecip':          [0],  # , 5, 10],
+    'roidist_weight':  [0],  # 1,]
+    'recip_weight':    [0],  # 1,]
+    'bursty_weight':   [0],  # 1,]
+    'ratio_weight':    [0, 1],  # 1,]
+    'lnbnn_weight':    [0, 1],  # 1,]
+    'lnrat_weight':    [0, 1],  # 1,]
+    'roidist_thresh':  [None],  # .5,]
+    'recip_thresh':    [0],  # 0
+    'bursty_thresh':   [None],  #
+    'ratio_thresh':    [None],  # 1.2, 1.6
+    'lnbnn_thresh':    [None],  #
+    'lnrat_thresh':    [None],  #
+    'nShortlist':      [1000],
+    'sv_on':           [True],  # True, False],
+    'score_method':    ['csum'],
+    'max_alts':        [1000],
 }
 
-vsmany_3456 = {
-    'query_type'     : ['vsmany'],
-    'checks'         : [256, 1024],#, 8192],
-    'K'              : [5, 10, 30], #5, 10],
-    'Knorm'          : [1, 3, 5], #2, 3],
-    'Krecip'         : [0, 1, 5, 10], #, 5, 10],
-    'roidist_weight' : [0], # 1,]
-    'recip_weight'   : [0], # 1,]
-    'bursty_weight'  : [0], # 1,]
-    'ratio_weight'   : [0], # 1,]
-    'lnbnn_weight'   : [0,1], # 1,]
-    'lnrat_weight'   : [0,1], # 1,]
-    'roidist_thresh' : [None, .5], # .5,]
-    'recip_thresh'   : [0], # 0
-    'bursty_thresh'  : [None], #
-    'ratio_thresh'   : [None], # 1.2, 1.6
-    'lnbnn_thresh'   : [None], #
-    'lnrat_thresh'   : [None], #
-    'nShortlist'   : [500],
-    'sv_on'        : [True], #True, False],
-    'score_method' : ['pl', 'plw', 'csum'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
-    'max_alts'     : [200, 600],
-}
 
 vsone_1 = {
-    'query_type'     : ['vsone'],
-    'checks'         : [256],#, 8192],
-    'K'              : [1], #5, 10],
-    'Knorm'          : [1], #2, 3],
-    'Krecip'         : [0], #, 5, 10],
-    'roidist_weight' : [0], # 1,]
-    'recip_weight'   : [0], # 1,]
-    'bursty_weight'  : [0], # 1,]
-    'ratio_weight'   : [1], # 1,]
-    'lnbnn_weight'   : [0], # 1,]
-    'lnrat_weight'   : [0], # 1,]
-    'roidist_thresh' : [None], # .5,]
-    'recip_thresh'   : [0], # 0
-    'bursty_thresh'  : [None], #
-    'ratio_thresh'   : [1.5], # 1.2, 1.6
-    'lnbnn_thresh'   : [None], #
-    'lnrat_thresh'   : [None], #
-    'nShortlist'   : [1000],
-    'sv_on'        : [True], #True, False],
-    'score_method' : ['csum'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
-    'max_alts'     : [500],
+    'query_type':      ['vsone'],
+    'checks':          [256],  # , 8192],
+    'K':               [1],  # 5, 10],
+    'Knorm':           [1],  # 2, 3],
+    'Krecip':          [0],  # , 5, 10],
+    'roidist_weight':  [0],  # 1,]
+    'recip_weight':    [0],  # 1,]
+    'bursty_weight':   [0],  # 1,]
+    'ratio_weight':    [1],  # 1,]
+    'lnbnn_weight':    [0],  # 1,]
+    'lnrat_weight':    [0],  # 1,]
+    'roidist_thresh':  [None],  # .5,]
+    'recip_thresh':    [0],  # 0
+    'bursty_thresh':   [None],  #
+    'ratio_thresh':    [1.5],  # 1.2, 1.6
+    'lnbnn_thresh':    [None],  #
+    'lnrat_thresh':    [None],  #
+    'nShortlist':      [1000],
+    'sv_on':           [True],  # True, False],
+    'score_method':    ['csum'],  # , 'pl'],  #, 'nsum', 'borda', 'topk', 'nunique']
+    'max_alts':        [500],
 }
 
 vsone_std = {
-    'query_type'     : 'vsone',
-    'checks'         : 256,
-    'K'              : 1,
-    'Knorm'          : 1,
-    'Krecip'         : 0,
-    'ratio_weight'   : 1,
-    'lnbnn_weight'   : 0,
-    'ratio_thresh'   : 1.5,
+    'query_type':    'vsone',
+    'checks':        256,
+    'K':             1,
+    'Knorm':         1,
+    'Krecip':        0,
+    'ratio_weight':  1,
+    'lnbnn_weight':  0,
+    'ratio_thresh':  1.5,
 }
 
 vsmany_scoremethod = {
-    'query_type'     : ['vsmany'],
-    'checks'         : [1024],#, 8192],
-    'K'              : [5], #5, 10],
-    'Knorm'          : [1], #2, 3],
-    'Krecip'         : [0], #, 5, 10],
-    'roidist_weight' : [0], # 1,]
-    'recip_weight'   : [0], # 1,]
-    'bursty_weight'  : [0], # 1,]
-    'ratio_weight'   : [0], # 1,]
-    'lnbnn_weight'   : [1], # 1,]
-    'lnrat_weight'   : [0], # 1,]
-    'roidist_thresh' : [None], # .5,]
-    'recip_thresh'   : [0], # 0
-    'bursty_thresh'  : [None], #
-    'ratio_thresh'   : [None], # 1.2, 1.6
-    'lnbnn_thresh'   : [None], #
-    'lnrat_thresh'   : [None], #
-    'nShortlist'   : [1000],
-    'sv_on'        : [True], #True, False],
-    'score_method' : ['csum', 'pl', 'plw', 'borda'],# 'bordaw', 'topk', 'topkw'],#, 'nsum', 'borda', 'topk', 'nunique']
-    'max_alts'     : [1000],
+    'query_type':      ['vsmany'],
+    'checks':          [1024],  # , 8192],
+    'K':               [5],  # 5, 10],
+    'Knorm':           [1],  # 2, 3],
+    'Krecip':          [0],  # , 5, 10],
+    'roidist_weight':  [0],  # 1,]
+    'recip_weight':    [0],  # 1,]
+    'bursty_weight':   [0],  # 1,]
+    'ratio_weight':    [0],  # 1,]
+    'lnbnn_weight':    [1],  # 1,]
+    'lnrat_weight':    [0],  # 1,]
+    'roidist_thresh':  [None],  # .5,]
+    'recip_thresh':    [0],  # 0
+    'bursty_thresh':   [None],  #
+    'ratio_thresh':    [None],  # 1.2, 1.6
+    'lnbnn_thresh':    [None],  #
+    'lnrat_thresh':    [None],  #
+    'nShortlist':      [1000],
+    'sv_on':           [True],  # True, False],
+    'score_method':    ['csum', 'pl', 'plw', 'borda'],  # 'bordaw', 'topk', 'topkw'],  # , 'nsum', 'borda', 'topk', 'nunique']
+    'max_alts':        [1000],
 }
 
 vsmany_best = {
-    'query_type'     : ['vsmany'],
-    'checks'         : [1024],#, 8192],
-    'K'              : [4], #5, 10],
-    'Knorm'          : [1], #2, 3],
-    'Krecip'         : [0], #, 5, 10],
-    'roidist_weight' : [0], # 1,]
-    'recip_weight'   : [0], # 1,]
-    'bursty_weight'  : [0], # 1,]
-    'ratio_weight'   : [0], # 1,]
-    'lnbnn_weight'   : [.01], # 1,]
-    'lnrat_weight'   : [0], # 1,]
-    'roidist_thresh' : [None], # .5,]
-    'recip_thresh'   : [0], # 0
-    'bursty_thresh'  : [None], #
-    'ratio_thresh'   : [None], # 1.2, 1.6
-    'lnbnn_thresh'   : [None], #
-    'lnrat_thresh'   : [None], #
-    'xy_thresh'      : [.002],
-    'nShortlist'   : [1000],
-    'sv_on'        : [True], #True, False],
-    'score_method' : ['csum'],# 'bordaw', 'topk', 'topkw'],#, 'nsum', 'borda', 'topk', 'nunique']
-    'max_alts'     : [1000],
+    'query_type':      ['vsmany'],
+    'checks':          [1024],  # , 8192],
+    'K':               [4],  # 5, 10],
+    'Knorm':           [1],  # 2, 3],
+    'Krecip':          [0],  # , 5, 10],
+    'roidist_weight':  [0],  # 1,]
+    'recip_weight':    [0],  # 1,]
+    'bursty_weight':   [0],  # 1,]
+    'ratio_weight':    [0],  # 1,]
+    'lnbnn_weight':    [.01],  # 1,]
+    'lnrat_weight':    [0],  # 1,]
+    'roidist_thresh':  [None],  # .5,]
+    'recip_thresh':    [0],  # 0
+    'bursty_thresh':   [None],  #
+    'ratio_thresh':    [None],  # 1.2, 1.6
+    'lnbnn_thresh':    [None],  #
+    'lnrat_thresh':    [None],  #
+    'xy_thresh':       [.002],
+    'nShortlist':      [1000],
+    'sv_on':           [True],  # True, False],
+    'score_method':    ['csum'],  # 'bordaw', 'topk', 'topkw'],  # , 'nsum', 'borda', 'topk', 'nunique']
+    'max_alts':        [1000],
 }
 best = vsmany_best
 
 overnight = vsmany_best
 overnight.update({
-    'K': [5, 10, 3],
-    'Knorm': [5, 3, 1],
-    'normalizer_rule': ['name', 'last'],
-    'use_adaptive_scale': [True, False],
-    'score_method' : ['pl', 'plw', 'csum', 'borda', 'bordaw'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
+    'K':                   [5, 10, 3],
+    'Knorm':               [5, 3, 1],
+    'normalizer_rule':     ['name', 'last'],
+    'use_adaptive_scale':  [True, False],
+    'score_method':        ['pl', 'plw', 'csum', 'borda', 'bordaw'],  # , 'pl'],  #, 'nsum', 'borda', 'topk', 'nunique']
 })
 
 
@@ -174,56 +151,55 @@ vsmany_kenya.update({
 
 vsmany_score = vsmany_best.copy()
 vsmany_score.update({
-    'score_method' : ['csum', 'pl', 'plw', 'borda', 'bordaw'],
+    'score_method': ['csum', 'pl', 'plw', 'borda', 'bordaw'],
 })
 
 vsmany_nosv = vsmany_best.copy()
 vsmany_nosv.update({
-    'sv_on' : [False]
+    'sv_on': [False]
 })
 
 vsmany_sv = vsmany_best.copy()
 vsmany_sv.update({
-    'xy_thresh' : [None, .1, .01, .001, .002]
+    'xy_thresh': [None, .1, .01, .001, .002]
 })
 
 vsmany_k = vsmany_best.copy()
 vsmany_k.update({
-    'K' : [2, 5, 10, 30]
+    'K': [2, 5, 10, 30]
 })
 
 
 vsmany_big_social = vsmany_best.copy()
 vsmany_big_social.update({
-    'K'              : [5, 10, 20], #30], #5, 10],
-    'Knorm'          : [1, 3], #2, 3],
-    'Krecip'         : [0],#, 5], #, 5, 10],
-    'lnbnn_weight'   : [1], # 1,]
-    #'roidist_thresh' : [None, .5], # .5,]
-    'score_method' : ['pl', 'plw', 'csum', 'borda', 'bordaw'],#, 'pl'], #, 'nsum', 'borda', 'topk', 'nunique']
+    'K':                [5, 10, 20],
+    'Knorm':            [1, 3],
+    'Krecip':           [0],
+    'lnbnn_weight':     [1],
+    'score_method':     ['pl', 'plw', 'csum', 'borda', 'bordaw'],
 })
 
 
 vsmany_1 = {
-    'query_type'     : ['vsmany'],
-    'checks'         : [1024],#, 8192],
-    'K'              : [5], #5, 10],
-    'Knorm'          : [1], #2, 3],
-    'Krecip'         : [0], #, 5, 10],
-    'roidist_weight' : [0], # 1,]
-    'recip_weight'   : [0], # 1,]
-    'bursty_weight'  : [0], # 1,]
-    'ratio_weight'   : [0], # 1,]
-    'lnbnn_weight'   : [1], # 1,]
-    'lnrat_weight'   : [0], # 1,]
-    'roidist_thresh' : [None], # .5,]
-    'recip_thresh'   : [0], # 0
-    'bursty_thresh'  : [None], #
-    'ratio_thresh'   : [None], # 1.2, 1.6
-    'lnbnn_thresh'   : [None], #
-    'lnrat_thresh'   : [None], #
-    'nShortlist'   : [1000],
-    'sv_on'        : [True], #True, False],
-    'score_method' : ['csum'],#, 'nsum', 'borda', 'topk', 'nunique']
-    'max_alts'     : [1000],
+    'query_type':      ['vsmany'],
+    'checks':          [1024],  # , 8192],
+    'K':               [5],  # 5, 10],
+    'Knorm':           [1],  # 2, 3],
+    'Krecip':          [0],  # , 5, 10],
+    'roidist_weight':  [0],  # 1,]
+    'recip_weight':    [0],  # 1,]
+    'bursty_weight':   [0],  # 1,]
+    'ratio_weight':    [0],  # 1,]
+    'lnbnn_weight':    [1],  # 1,]
+    'lnrat_weight':    [0],  # 1,]
+    'roidist_thresh':  [None],  # .5,]
+    'recip_thresh':    [0],  # 0
+    'bursty_thresh':   [None],  #
+    'ratio_thresh':    [None],  # 1.2, 1.6
+    'lnbnn_thresh':    [None],  #
+    'lnrat_thresh':    [None],  #
+    'nShortlist':      [1000],
+    'sv_on':           [True],  # True, False],
+    'score_method':    ['csum'],  # , 'nsum', 'borda', 'topk', 'nunique']
+    'max_alts':        [1000],
 }

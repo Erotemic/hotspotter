e58e759dabef7fa122d7ab12b5fdb0735686012d
Jason Parham <bluemellophone@gmail.com>
2014-02-06 12:52:54 -0500

    Updated mac dmg script to prevent errors

--- _setup/mac_dmg_builder.sh
+++ _setup/mac_dmg_builder.sh
@@ -35,11 +35,11 @@ echo '
              set current view of container window to icon view
              set toolbar visible of container window to false
              set statusbar visible of container window to false
-             set the bounds of container window to {400, 100, 885, 430}
              set theViewOptions to the icon view options of container window
              set arrangement of theViewOptions to not arranged
              set icon size of theViewOptions to 72
              set background picture of theViewOptions to file ".background:'${hs_backgroundPictureName}'"
+             set the bounds of container window to {400, 100, 885, 430}
              make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
              set position of item "'${hs_applicationName}'" of container window to {100, 100}
              set position of item "Applications" of container window to {375, 100}
